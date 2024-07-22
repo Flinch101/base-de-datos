d583253a5546336ea36110e6189b28be8b7ecdb5
devflinch <72756232+Flinch101@users.noreply.github.com>
2024-07-22 17:34:32 -0300

    Update base-de-datos.js

--- base-de-datos.js
+++ base-de-datos.js
@@ -3,7 +3,10 @@ var publicaciones = [
         /* Nombre */ "Parlante TG 113 Bluetooth",
         /* Precio */ "$50.000",
         [ /* Nombre */ 
-            "./productos/producto_1_1.png","./productos/producto_1_2.png","./productos/producto_1_3.png","./productos/producto_1_4.png",
+            "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_1_1.png",
+            "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_1_2.png",
+            "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_1_3.png",
+            "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_1_4.png",
         ],
         /* Descripcion */ "",
         /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Parlante%20TG%20113!"
@@ -12,7 +15,7 @@ var publicaciones = [
         /* Nombre */ "Portalampara Adaptador E27 Tecla Enchufe Velador Luz Noche Color Blanco",
         /* Precio */ "$1.500",
         [ /* Nombre */ 
-            "./productos/producto_2_1.webp"
+            "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_2_1.webp"
         ],
         /* Descripcion */ "• Alta calidad, durable y estable\n• Zócalo E27\n• Adaptador de enchufe para Estados Unidos\n• Fácil instalación y uso seguro\n• Resistencia a altas temperaturas\n• Interruptor de encendido y apagado\n• Tensión de trabajo: 100V-240V\n• Material: PVC + Metal\n• Color: Blanco\n• Dimensiones: 67 x 47 x 45 mm\n• Origen: China",
         /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Portalampara%20Adaptador%20E27%20Tecla%20Enchufe%20Velador%20Luz%20Noche%20Color%20Blanco!"
