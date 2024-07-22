8df3437b80a94baffdb349ff16ffd3ff4fc3b33a
devflinch <72756232+Flinch101@users.noreply.github.com>
2024-07-22 18:24:06 -0300

    Update base-de-datos.js

--- base-de-datos.js
+++ base-de-datos.js
@@ -3,10 +3,10 @@
         /* Nombre */ "Parlante TG 113 Bluetooth",
         /* Precio */ "$50.000",
         [ /* Nombre */ 
-            "https://raw.githubusercontent.com/Flinch101/base-de-datos/blob/main/productos/producto_1_1.png",
-            "https://raw.githubusercontent.com/base-de-datos/blob/main/productos/producto_1_2.png",
-            "https://raw.githubusercontent.com/base-de-datos/blob/main/productos/producto_1_3.png",
-            "https://raw.githubusercontent.com/Flinch101/base-de-datos/blob/main/productos/producto_1_4.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_1.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_2.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_3.png",
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_1_4.png",
         ],
         /* Descripcion */ "",
         /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Parlante%20TG%20113!",
@@ -17,7 +17,7 @@
         /* Nombre */ "Portalampara Adaptador E27",
         /* Precio */ "$1.500",
         [ /* Nombre */ 
-            "https://raw.githubusercontent.com/Flinch101/base-de-datos/blob/main/productos/producto_2_1.png"
+            "https://raw.githubusercontent.com/Flinch101/base-de-datos/main/productos/producto_2_1.png"
         ],
         /* Descripcion */ "• Alta calidad, durable y estable\n• Zócalo E27\n• Adaptador de enchufe para Estados Unidos\n• Fácil instalación y uso seguro\n• Resistencia a altas temperaturas\n• Interruptor de encendido y apagado\n• Tensión de trabajo: 100V-240V\n• Material: PVC + Metal\n• Color: Blanco\n• Dimensiones: 67 x 47 x 45 mm\n• Origen: China",
         /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Portalampara%20Adaptador%20E27%20Tecla%20Enchufe%20Velador%20Luz%20Noche%20Color%20Blanco!",
