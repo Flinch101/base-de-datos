a94b0fcccd892366d364822c325e13b390f8cd91
devflinch <72756232+Flinch101@users.noreply.github.com>
2024-07-22 17:56:25 -0300

    Update base-de-datos.js

--- base-de-datos.js
+++ base-de-datos.js
@@ -9,7 +9,8 @@
             "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_1_4.png",
         ],
         /* Descripcion */ "",
-        /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Parlante%20TG%20113!"
+        /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Parlante%20TG%20113!",
+        /* Nuevo/Usado */ "nuevo"
     ],
     [
         /* Nombre */ "Portalampara Adaptador E27 Tecla Enchufe Velador Luz Noche Color Blanco",
@@ -18,6 +19,7 @@
             "https://github.com/Flinch101/base-de-datos/blob/main/productos/producto_2_1.webp"
         ],
         /* Descripcion */ "• Alta calidad, durable y estable\n• Zócalo E27\n• Adaptador de enchufe para Estados Unidos\n• Fácil instalación y uso seguro\n• Resistencia a altas temperaturas\n• Interruptor de encendido y apagado\n• Tensión de trabajo: 100V-240V\n• Material: PVC + Metal\n• Color: Blanco\n• Dimensiones: 67 x 47 x 45 mm\n• Origen: China",
-        /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Portalampara%20Adaptador%20E27%20Tecla%20Enchufe%20Velador%20Luz%20Noche%20Color%20Blanco!"
+        /* Link WhatsApp */ "https://api.whatsapp.com/send?phone=+5491161612430&text=Hola%20buenas,%20estoy%20interesad@%20en%20el%20Portalampara%20Adaptador%20E27%20Tecla%20Enchufe%20Velador%20Luz%20Noche%20Color%20Blanco!",
+        /* Nuevo/Usado */ "usado"
     ],
 ]
